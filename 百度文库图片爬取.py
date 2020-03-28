22323f7058534688a866104f1ba6c64a41c01d62
youxuezhe7 <62144799+youxuezhe7@users.noreply.github.com>
2020-03-28 10:02:19 +0800

    Add files via upload
    获取百度文库中的图片并储存在文档中

---
+++ 百度文库图片爬取.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+if __name__ == '__main__':
+    url=r'https://wenku.baidu.com/browse/getbcsurl?doc_id=378f7303f01dc281e53af0ac&pn=1&rn=99999&type=ppt&callback=jQuery110108441270703745489_1585356567452&_=1585356567453'
+    header={
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+    }
+    response=requests.get(url=url,headers=header)
+    req=response.text
+    # print(req)
+    req1=req.strip('()jQuery110108441270703745489_1585356567452')        #去掉小括号
+    # print(req1)
+    result1=json.loads(req1)
+    # print(result)
+    result2=result1['list']
+    # print(result2)
+    for i in result2:
+        final=i['zoom']
+        # print(final)
+        final1=requests.get(url=final,headers=header)
+        pickture=final1.content
+        number=final.rsplit('=',1)[1]
+        with open(r'C:\Users\User\Desktop\百度文库\{}.jpg'.format(number),'ab+') as fp:
+            fp.write(pickture)
+            print('爬取成功')
