47b2d9e3c481b57620c613728a74df4f8ca56317
youxuezhe7 <62144799+youxuezhe7@users.noreply.github.com>
2020-03-28 10:14:19 +0800

    Update 百度文库图片爬取.py
    需要先通过抓包找出json数据的url

--- 百度文库图片爬取.py
+++ 百度文库图片爬取.py
@@ -3,6 +3,7 @@ import json
 
 if __name__ == '__main__':
     url=r'https://wenku.baidu.com/browse/getbcsurl?doc_id=378f7303f01dc281e53af0ac&pn=1&rn=99999&type=ppt&callback=jQuery110108441270703745489_1585356567452&_=1585356567453'
+    #需先找到json数据的真实url
     header={
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
     }
@@ -20,7 +21,7 @@ if __name__ == '__main__':
         # print(final)
         final1=requests.get(url=final,headers=header)
         pickture=final1.content
-        number=final.rsplit('=',1)[1]
+        number=final.rsplit('=',1)[1]   #将图片名切片
         with open(r'C:\Users\User\Desktop\百度文库\{}.jpg'.format(number),'ab+') as fp:
             fp.write(pickture)
             print('爬取成功')
